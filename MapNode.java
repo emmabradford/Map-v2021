7734d8a8e7d8e6821261d47bcec5fdf0e6f8e82f
Emma Bradford <ebradford@Emmas-MacBook-Air.local>
2021-05-26 09:38:59 -0500

    added mapNode that implements doubly linked node

---
+++ MapNode.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class MapNode here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MapNode<K, V> extends Item<K, V> implements DoublyLinkedNode<K, V>
+{
+    
+    public MapNode(K key, V value, DoublyLinkedNode<K, V> previous, DoublyLinkedNode<K, V> next)
+    {
+        super(key, value);
+    }
+   /**
+     * Returns the previous (upstream) node.
+     */
+    public DoublyLinkedNode<K, V> getPrevious()
+    {
+        return null;
+    }
+
+    /**
+     * Sets the previous node.
+     */
+    public void setPrevious(DoublyLinkedNode<K, V> node)
+    {
+        
+    }
+
+    /**
+     * Returns the next (downstream) node.
+     */
+    public DoublyLinkedNode<K, V> getNext()
+    {
+        return null;
+    }
+
+    /**
+     * Sets the next (downstream) node.
+     */
+    public void setNext(DoublyLinkedNode<K, V> node)
+    {
+    
+    }
+}
